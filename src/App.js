7af06860fe31c0c36f73f7e5df6d0f900ec97f20
My8921 <142293908+My8921@users.noreply.github.com>
2023-10-13 15:33:23 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Home from "../src/Components/pages/Home/Home";
+
+function App() {
+  return (
+    <div className="App ">
+      <Home />
+   
+    </div>
+  );
+}
+
+export default App;
